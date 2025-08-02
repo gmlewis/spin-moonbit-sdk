e82fdf44aa8e09dd0db9b3589ab910cf4afbf97a
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-08-02 11:41:45 -0400

    Rebuild with wit-bindgen-cli 0.41.0
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- wit/run-wit-deps.sh
+++ wit/run-wit-deps.sh
@@ -1,4 +1,8 @@
 #!/bin/bash -ex
-rm -rf ./deps
+rm -rf ./deps ./deps.lock
 cargo install wit-deps-cli
-pushd .. && wit-deps
+pushd .. && wit-deps && popd
+pushd deps
+rep float32 f32 `rg -l float32`
+rep float64 f64 `rg -l float64`
+popd
