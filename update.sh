aa399606942e5d975d2bfc212821ac6aca4e3b6d
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-02-10 19:58:54 -0500

    Update to moonc v0.8.1+bd827dc85
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-moon update && moon install && rm -rf target
+moon update && rm -rf target
 moon fmt && moon info
 # As of moonc v0.6.31+b5b06ff93, `--target native` no longer works.
 # moon test --target all
